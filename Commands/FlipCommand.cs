4ba220351190113f23f79c2532eb31039194003e
ant0niy07 <86295276+ant0niy07@users.noreply.github.com>
2023-06-11 14:51:55 +0200

    Add files via upload

---
+++ Commands/FlipCommand.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Catel.MVVM;
+
+namespace ConvolutionWpf.Commands
+{
+    public class FlipCommand : Command
+    {
+        private readonly Func<WriteableBitmap> _imageFactory;
+
+        public event Action<WriteableBitmap> OnImageChanged;
+
+        public FlipCommand(Func<WriteableBitmap> imageFactory)
+            : base(() => { })
+        {
+            _imageFactory = imageFactory;
+        }
+
+        public void ExecuteCommand()
+        {
+            var image = _imageFactory();
+            if (image == null)
+                return;
+
+            var pixels = new byte[image.PixelHeight * image.BackBufferStride];
+            image.CopyPixels(pixels, image.BackBufferStride, 0);
+
+            var imageRes = new WriteableBitmap(2 * image.PixelWidth, image.PixelHeight, image.DpiX, image.DpiY, image.Format, image.Palette);
+            var resultPixels = new byte[imageRes.PixelHeight * imageRes.BackBufferStride];
+
+            int bytesPerPixel = (image.Format.BitsPerPixel0 + 7) / 8;
+
+            for (int y = 0; y < image.PixelHeight; y++)
+            {
+                for (int x = 0; x < image.PixelWidth; x++)
+                {
+                    int srcIndex = y * image.BackBufferStride + x * bytesPerPixel;
+                    int destIndex = y * imageRes.BackBufferStride + (imageRes.PixelWidth - x - 1) * bytesPerPixel;
+
+                    for (int b = 0; b < bytesPerPixel; b++)
+                    {
+                        resultPixels[destIndex + b] = pixels[srcIndex + b];
+                    }
+                }
+            }
+
+            imageRes.WritePixels(new Int32Rect(0, 0, imageRes.PixelWidth, imageRes.PixelHeight), resultPixels, imageRes.BackBufferStride, 0);
+
+            OnImageChanged?.Invoke(imageRes);
+        }
+
+        protected override void Execute(object parameter, bool ignoreCanExecuteCheck)
+        {
+            ExecuteCommand();
+        }
+    }
+}
